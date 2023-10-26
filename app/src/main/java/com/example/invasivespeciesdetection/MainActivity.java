7aecbb80d56cc9a75eeae7e1772b1476866b8616
harouna <harounathaim2005@gmail.com>
2023-10-26 01:55:05 -0400

    fixed getMax()

--- app/src/main/java/com/example/invasivespeciesdetection/MainActivity.java
+++ app/src/main/java/com/example/invasivespeciesdetection/MainActivity.java
@@ -110,7 +110,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    int getMax(float[] arr) {
+    int getMax(float[] arr)  {
         int max = 0;
         for (int i = 0; i < arr.length; i++) {
                 if (arr[i] > arr[max]) {
