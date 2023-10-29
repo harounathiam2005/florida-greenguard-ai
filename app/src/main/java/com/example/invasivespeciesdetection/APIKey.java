74a6dfde6431fcdcb45a072bda017bb41607ebbc
Harouna <harounathiam2005@gmail.com>
2023-10-29 18:01:10 -0400

    Removed APIKey from public domain

--- app/src/main/java/com/example/invasivespeciesdetection/APIKey.java
+++
@@ -1,9 +0,0 @@
-package com.example.invasivespeciesdetection;
-
-public class APIKey {
-
-    public static String getAPIKey() {
-        return "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";
-    }
-
-}
